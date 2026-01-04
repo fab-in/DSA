69b9fef3c75981a1f28ed30e67d383b1821fa1c8
fabin <fabinchandi@gmail.com>
2026-01-04 19:22:20 +0530

    pattern7 done

---
+++ Patterns/pattern7.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    for j in range(5-i-1):
+        print(" ",end="")
+    for k in range(i*2+1):
+        print("*",end="")
+    for j in range(5-i-1):
+        print(" ",end="")
+    print()
