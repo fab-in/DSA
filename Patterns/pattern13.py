545c83c6df724c1ae1a4c82eb989d82c2082d8a1
fabin <fabinchandi@gmail.com>
2026-05-26 22:52:30 +0530

    fresh start

--- Patterns/pattern13.py
+++
@@ -1,6 +0,0 @@
-count=1
-for i in range(6):
-    for j in range(1,i):
-        print(count,end=" ")
-        count+=1
-    print()
--- Patterns/pattern7.py
+++
@@ -1,8 +0,0 @@
-for i in range(5):
-    for j in range(5-i-1):
-        print(" ",end="")
-    for k in range(i*2+1):
-        print("*",end="")
-    for j in range(5-i-1):
-        print(" ",end="")
-    print()
